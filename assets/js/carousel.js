92f2a71b93a430fd509fda23743b59bb2b6fe644
TranManhGiang <tranmanhgiang06051999@gmail.com>
2020-07-22 14:55:53 +0700

    responsive

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin: 30,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:2,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:true
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 30,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true
+            }
         }
-    }
-})            
+    })            
 });
